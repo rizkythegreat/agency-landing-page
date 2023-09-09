5178a4915f31ad3aed863dd6c53456f34506bebd
Rizky Rahman Salam <clashikik@gmail.com>
2023-09-09 22:10:49 +0700

    adding fontFamily to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     ],
     theme: {
         extend: {
+            fontFamily: {
+                'sans': ['"Open Sans"', 'Arial', 'sans-serif']
+            },
+
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
