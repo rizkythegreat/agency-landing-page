5c3adb1268b7e8dc18e60d4f5098f7214ce09109
Rizky Rahman Salam <clashikik@gmail.com>
2023-09-10 15:26:18 +0700

    add responsive navbar

---
+++ components/homepage/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import logoSVG from '../../public/assets/logo/logo.svg'
+import Image from 'next/image';
+import Link from 'next/link';
+import Header from '../Layout/Header';
+
+
+function HomeNav() {
+
+
+    return (
+        <>
+            <div className=''>
+                <Header/>
+            </div>
+        </>
+    )
+}
+
+export default HomeNav;
